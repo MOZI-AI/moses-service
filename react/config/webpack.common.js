f9c924c35ecd20325031a5d24062c023545d2d75
Abdulrahman Semrie <hsamireh@gmail.com>
2018-12-07 19:39:52 +0300

    Added webpack production build configs
    **Created a Dockerfile for the front-end

---
+++ react/config/webpack.common.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = require('./paths.js');
+module.exports = {
+
+  entry: path.join(paths.srcPath, 'index.js'),
+
+  output: {
+    path: paths.outPath,
+    filename: 'bundle.[chunkhash:8].js'
+  },
+
+  devtool: 'source-map',
+  devServer: {
+    contentBase: paths.outPath,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: paths.srcPath,
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ['es2015', { modules: false }], 'react'
+          ],
+          plugins: [
+            ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "less-loader",
+          options: {
+            javascriptEnabled: true
+          }
+        }]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[hash].[ext]",
+          },
+        },
+      },
+    ]
+  },
+
+  plugins: [
+    // TODO Remove this if there is no need for it
+    // new HtmlWebpackPlugin({
+    //   inject: true,
+    //   template: path.join(paths.srcPath, 'index.html'),
+    //   minify: {
+    //     removeComments: true,
+    //     collapseWhitespace: true,
+    //     removeRedundantAttributes: true,
+    //     useShortDoctype: true,
+    //     removeEmptyAttributes: true,
+    //     removeStyleLinkTypeAttributes: true,
+    //     keepClosingSlash: true,
+    //     minifyJS: true,
+    //     minifyCSS: true,
+    //     minifyURLs: true,
+    //   },
+    // }),
+  ]
+};
---
+++ react/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  plugins: [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new UglifyJSPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        compress: true,
+        mangle: true,
+        warnings: false
+      }
+    }),
+    new CompressionPlugin({
+      test: /\.js/,
+      filename (asset) {
+        return asset.replace('.gz', '')
+      }
+    })
+  ]
+});
