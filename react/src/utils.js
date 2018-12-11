6cf67033795072a2c2f01a7beea5a67d7af05c45
Yisehak Abreham <a.yisehak@outlook.com>
2018-12-11 16:06:06 +0300

    attempts to read service address from enviroment variable , falls back to localhost

--- react/src/utils.js
+++ react/src/utils.js
@@ -4,4 +4,5 @@ export const AnalysisStatus = {
   ERROR: -1
 };
 
-export const SERVER_ADDRESS = "http://localhost:5000/";
+export const SERVER_ADDRESS =
+  process.env.SERVICE_ADDR || "http://localhost:5000/";
